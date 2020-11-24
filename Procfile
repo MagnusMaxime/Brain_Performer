70a738c1efd2c93e3964f50dc437dd0ed32a7421
Marc Partensky <marc.partensky@gmail.com>
2020-11-25 00:53:13 +0100

    changed procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: vendor/bin/heroku-php-apache2 web/
+web: cd src; php -S localhost:8000
+web1: vendor/bin/heroku-php-apache2 web/
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 class HomeController extends Controller
 {
     static public function index(){
+        global $twig;
         return $twig->render('index.html', []);
     }
     public function show(){
