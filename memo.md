eee375e0e03a7acbbf2d893e696eb10d3505d43e
Marc Partensky <marc.partensky@gmail.com>
2020-12-01 16:21:21 +0100

    memo.md

--- memo.md
+++ memo.md
@@ -1,3 +1,2 @@
 Pour lancer le serveur php, tapez dans le terminal IntelliJ la commande suivante :
 cd src & php -S localhost:8000
-
--- src/Router/Route.php
+++ src/Router/Route.php
@@ -10,13 +10,12 @@ class Route
 
     public function __construct($path, $callable)
     {
-        $this->path = trim($path,'/');
+        $this->path = trim($path, '/');
         $this->callable = $callable;
     }
     public function match($url){
         $url = trim($url, '/');
         $path = preg_replace('#:([\w]+)#','([^/]+)', $this->path);#C'est pour prendre en compte les paramètres
-
 
         if (!preg_match("#^$path$#i", $url, $matches)){ #vive les regex, drapeau i pour être sensible à la casse
             return false;
