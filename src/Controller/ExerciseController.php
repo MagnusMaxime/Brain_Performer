dde720c5536d6fe0a254790a3fa660ba8a9b0c9c
Spaectus <alex-ad.bigot@laposte.net>
2021-01-28 19:32:42 +0100

    exercise part1

--- src/Controller/ExerciseController.php
+++ src/Controller/ExerciseController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 
 use App\Model\Exercise;
+use App\Model\User;
 
 class ExerciseController extends Controller
 {
@@ -16,6 +17,21 @@ class ExerciseController extends Controller
     static public function one($id){
         global $twig;
         $data=Exercise::getExercise($id);
+        if ($data["public"]=="0"){
+            //l'exercice est privé
+            if (self::needToBeConnected()){//l'utilisateur est pas connecté et a besoin d'être connecté
+                return "";
+            }
+            if ($data["owner"]!=$_SESSION["id"]){
+                $user = new User($data["owner"]);
+                $user_info=$user->get_info();
+                if ($user_info["parent"]!=$_SESSION["id"]){
+                    //ce n'est pas le médecin du patient, on bloque
+                    header("Location: /");
+                    return "";
+                }
+            }
+        }
         if ($data===false){
             //l'exercice n'existe pas
             header("Location: /");
@@ -24,6 +40,20 @@ class ExerciseController extends Controller
         $data["title"]="Mon exercice";
         //var_dump($data);
         return $twig->render('exercise.html', $data);
+
+
+    }
+
+    static public function user(){
+        global $twig;
+        if (self::needToBeConnected()){
+            //l'utilisateur est pas connecté et a besoin d'être connecté
+            return "";
+        }
+        $twig_array=[];
+        $twig_array["exercises"]=Exercise::getExerciseOfUser($_SESSION["id"]);
+        $twig_array["title"]="Mes exercices";
+        return $twig->render('exercises.html', $twig_array);
     }
 
 
